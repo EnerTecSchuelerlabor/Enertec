4dfa84abcb980914f4c53e8e0f61cb6edcb11fb4
EnerTecSchuelerlabor <enertec@uni-saarland.de>
2026-04-21 22:14:09 +0200

    Update solar_main.py

--- pages/solar_main.py
+++ pages/solar_main.py
@@ -10,18 +10,18 @@ st.title("Solarenergieexperiment")
 
 st.write("Hier ist die Solar-Hauptseite.")
 
-#st.logo('Logo_EnerTec_ohne_Hintergrund.png')
+st.logo('Solar/Logo_EnerTec_ohne_Hintergrund.png')
 
-#col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns(3)
 
-#with col1:
-#    st.image('Logo_Stiftung_ohne_Hintergrund.png', width=250)
+with col1:
+    st.image('Solar/Logo_Stiftung_ohne_Hintergrund.png', width=250)
     
-#with col2:
-#    st.image('Logo_EnerTec_ohne_Hintergrund.png', width=250)
+with col2:
+    st.image('Solar/Logo_EnerTec_ohne_Hintergrund.png', width=250)
     
-#with col3:
-#    st.image('Logo_AES_ohne_Hintergrund.png', width=250)
+with col3:
+    st.image('Solar/Logo_AES_ohne_Hintergrund.png', width=250)
     
 # ------------------------------------------------------------------------------------------------------------------------ #
 
