0fc8770ebb8641c88f60e8ef3500915380e4fa3b
EnerTecSchuelerlabor <enertec@uni-saarland.de>
2026-04-21 21:50:58 +0200

    Update s11_Aufbau_Versuchskoffer.py

--- pages/s11_Aufbau_Versuchskoffer.py
+++ pages/s11_Aufbau_Versuchskoffer.py
@@ -35,31 +35,31 @@ st.write('---')
 
 st.write('Auf dem ersten Bild seht ihr den geöffneten Koffer mit Schaumstoff im Deckel')
 
-st.image('page_11_pic_1.png')
+st.image('Solar/page_11_pic_1.png')
 
 st.write('---')
 
 st.write('Im Deckel des Koffers ist die Grundplatte verstaut (siehe Bild unten). Nehmt diese aus dem Koffer und platziert sie auf dem Tisch. Richtet sie so aus, dass die Rundung nach links zeigt. So könnt ihr später einfacher die Winkelskala verwenden.')
 
-st.image('page_11_pic_2.png')
+st.image('Solar/page_11_pic_2.png')
 
 st.write('---')
 
 st.write('Im nächsten Bild seht ihr einmal alle Komponenten des Koffers. Das Poster an der Wand enthält eine beschriftete Version.')
 
-st.image('page_11_pic_7.png')
+st.image('Solar/page_11_pic_7.png')
 
 st.write('---')
 
 st.write('Auf dem nächsten Bild seht ihr die Grundplatte mit markierten Bohrungen. Die rot markierte Bohrung nimmt dabei wie man sieht den Lampenarm auf. Die blau markierten Bohrungen sind im nächsten Schritt zur Montage des Solarmoduls gedacht.')
 
-st.image('page_11_pic_3.png')
+st.image('Solar/page_11_pic_3.png')
 
 st.write('---')
 
 st.write('Im folgenden Bild seht ihr das fertig montierte Solarmodul. Achtet darauf, dass die Winkelskala zu euch zeigt. Die Rundung der Grundplatte sollte dafür nach links zeigen.')
 
-st.image('page_11_pic_4.png')
+st.image('Solar/page_11_pic_4.png')
 
 st.write('---')
 
@@ -68,7 +68,7 @@ st.write('Hier seht ihr den fertigen Aufbau mit und ohne den Anschlüssen für d
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('page_11_pic_5.png')
+    st.image('Solar/page_11_pic_5.png')
     
 with col2:
     st.image('page_11_pic_6.png')
