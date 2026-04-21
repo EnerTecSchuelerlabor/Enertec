1407c170f3fc245cacfae4f6b9f39d415f1cb3d6
EnerTecSchuelerlabor <enertec@uni-saarland.de>
2026-04-21 22:10:17 +0200

    Update wind_main.py

--- pages/wind_main.py
+++ pages/wind_main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from navigation import hide_default_sidebar_nav, solar_sidebar
+from navigation import hide_default_sidebar_nav, wind_sidebar
 
 st.set_page_config(page_title="Wind", layout="wide")
 
