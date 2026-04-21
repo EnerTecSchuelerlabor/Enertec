52de5770b4a615207b82b6ac0130e0cdfc8ec601
EnerTecSchuelerlabor <enertec@uni-saarland.de>
2026-04-21 21:48:05 +0200

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -31,13 +31,13 @@ def solar_sidebar():
             st.switch_page("pages/s3_Versuch_3.py")
 
         if st.button("Versuch 4", use_container_width=True):
-            st.switch_page("pages/s1_Versuch_4.py")
+            st.switch_page("pages/s4_Versuch_4.py")
 
         if st.button("Versuch 5", use_container_width=True):
-            st.switch_page("pages/s2_Versuch_5.py")
+            st.switch_page("pages/s5_Versuch_5.py")
 
         if st.button("Versuch 6", use_container_width=True):
-            st.switch_page("pages/s3_Versuch_6.py")
+            st.switch_page("pages/s6_Versuch_6.py")
 
         st.divider()
 
